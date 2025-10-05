c9cf40a6d7ab63cd9c03ad5c8d6a8f74eb395726
nidvar <djayshine87@hotmail.com>
2025-10-05 15:18:54 +1100

    navbar css changes

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './assets/global.css'
 
 // Import Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+// Custom CSS
+import './assets/global.css'
 
 const app = createApp(App)
 
